61b8a5a48dbf30e31a122145589f9d3763853db7
Monaliza Loren <83433815+monalizaloren@users.noreply.github.com>
2023-10-23 14:31:00 -0300

    Update Details.js

--- screens/Details.js
+++ screens/Details.js
@@ -10,57 +10,7 @@ import {
 import axios from "axios";
 
 export default class DetailsScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      details: {},
-      imagePath: "",
-      url: `https://2431-2405-201-8008-e095-a413-c282-beb3-6a86.ngrok.io/planet?name=${this.props.navigation.getParam(
-        "planet_name"
-      )}`,
-    };
-  }
-
-  componentDidMount() {
-    this.getDetails();
-  }
-  getDetails = () => {
-    const { url } = this.state;
-    axios
-      .get(url)
-      .then((response) => {
-        this.setDetails(response.data.data);
-      })
-      .catch((error) => {
-        Alert.alert(error.message);
-      });
-  };
-
-  setDetails = (planetDetails) => {
-    const planetType = planetDetails.planet_type;
-    let imagePath = "";
-    switch (planetType) {
-      case "Gas Giant":
-        imagePath = require("../assets/Gas_Giant.png");
-        break;
-      case "Terrestrial":
-        imagePath = require("../assets/Terrestrial.png");
-        break;
-      case "Super Earth":
-        imagePath = require("../assets/Super_Earth.png");
-        break;
-      case "Neptune Like":
-        imagePath = require("../assets/Neptune-like.png");
-        break;
-      default:
-        imagePath = require("../assets/Gas_Giant.png");
-    }
-
-    this.setState({
-      details: planetDetails,
-      imagePath: imagePath,
-    });
-  };
+  //PASSO 4
 
   render() {
     const { details, imagePath } = this.state;
